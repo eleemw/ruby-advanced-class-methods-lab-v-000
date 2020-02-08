6800c1770f00e05ff2f22a84e137740f46efe353
Erica Murdock-Waters <ericahpa@gmail.com>
2020-02-08 23:40:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,7 @@ class Song
 #  end
 
   def self.create
-    self.all << self.new   #instantiates and saves the song
+    self.all << self.new 
     @@all[-1]           #returns new song (last element added to @@all array)
   end
 
